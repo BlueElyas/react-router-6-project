b8a72ee70e44b35bc0290aa70b8754b3e627a627
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-12 15:54:19 +0000

    add miragejs api data to the vans component

--- src/Components/Vans.jsx
+++ src/Components/Vans.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function() {
+    const [vanData, setVanData] = useState([])
+
+    useEffect(() =>{
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => {
+                setVanData(data.vans)
+            })
+    } ,[])
+    console.log(vanData)
+
+    const vanEl = vanData.map(data => {
+        return(
+            <div key={data.id} className="van-tile">
+                <img src={data.imageUrl}  />
+                <div className="van-info">
+                    <p>{data.name}</p>
+                    <p>{data.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${data.type} selected`}>{data.type}</i>
+            </div>
+        )
+    })
+
     return(
         <>
-            <h1>Vans page goes here</h1>
+            <div className="van-list-container">
+                <h1>Explore our van options</h1>
+                <div className="van-list">
+                    {vanEl}
+                </div>
+            </div>
         </>
     )
 }
