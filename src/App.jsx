3de972782141ac8ef96f2e3ac7914572b962bc7a
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-13 11:13:04 +0000

    add layout element to app component and add default layout jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from "react"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
 import About from "./Pages/About"
 import Vans from "./Pages/Vans"
 import VanDetails from "./Pages/VanDetails"
+import Layout from "./Components/Layout"
 
 import "./server"
 
 export default function App() {
   return (
     <BrowserRouter>
-      
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/Vans" element={<Vans/>}/>
-        <Route path="/Vans/:id" element={<VanDetails/>}/>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Vans" element={<Vans/>}/>
+          <Route path="/Vans/:id" element={<VanDetails/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
