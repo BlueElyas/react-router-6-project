d0ca6b87d5f4feb4a3730310d0b7b71d52a8985d
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-13 11:39:48 +0000

    nest the routes for the host folder

--- src/Pages/Host/Dashboard.jsx
+++ src/Pages/Host/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Dashboard() {
     return(
         <>
             <h1>Host dashboard here</h1>
+            <Outlet/>
         </>
     )
 }
