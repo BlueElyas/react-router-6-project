4f5fbf040228234b3232a78012113b26794d8229
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-13 11:03:04 +0000

    add a header and layout component
    the header jsx inside the app component was moved into the new header component

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,7 @@ import "./server"
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/Vans">Vans</Link>
-        </nav>
-      </header>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
