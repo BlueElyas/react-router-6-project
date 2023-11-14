b550cb39b1ecfe4e6583cd0b5c3d0d47462a75bf
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-14 16:09:07 +0000

    add a relative link to vandetails component to return to previous component

--- src/Pages/Vans/VanDetails.jsx
+++ src/Pages/Vans/VanDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function VanDetails() {
     const params = useParams()
@@ -14,6 +14,11 @@ export default function VanDetails() {
     return(
         <>
             <div className="van-detail-container">
+            <Link
+                to=".."
+                relative="path"
+                className="back-button"
+            >&larr; <span>Back to all vans</span></Link>
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
