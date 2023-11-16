00335b6cbc98f81d58d1ee03d5d9f8b0ebbff726
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-11-16 13:21:14 +0000

    updatge footer and header components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 
 export default function Footer() {
-    return(
+    return (
         <footer>&#169; 2022 #VANLIFE</footer>
     )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,47 @@
 import React from "react"
 import { Link, NavLink } from "react-router-dom"
+import imageUrl from "/assets/images/avatar-icon.png"
 
 export default function Header() {
-
-    const styles = {
+    const activeStyles = {
         fontWeight: "bold",
         textDecoration: "underline",
-        color: "#161616",
+        color: "#161616"
     }
+
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <NavLink style={({isActive}) => isActive ? styles : null} to="/host">Host</NavLink>
-                <NavLink style={({isActive}) => isActive ? styles : null} to="about">About</NavLink>
-                <NavLink style={({isActive}) => isActive ? styles : null} to="vans">Vans</NavLink>
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
                 <Link to="login" className="login-link">
-                    <img 
-                        src="../assets/images/avatar-icon.png" 
+                    <img
+                        src={imageUrl}
                         className="login-icon"
                     />
                 </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
